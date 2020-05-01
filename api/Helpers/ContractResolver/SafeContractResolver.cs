6dc28762a1210131ddf98edaaadc2eb353463aa7
Travis Semple <travis.semple@quartech.com>
2020-05-01 08:07:40 -0700

    - Include jc-client-interface project - Add in converted RAML to OpenAPI V3 as oas3.json - Generate jc-clients using NSwagStudio - Add in SafeContractResolver - which allows nulls for properties on jc-client's models - Add Swashbuckle into the project, can be viewed at apiUrl/swagger/

---
+++ api/Helpers/ContractResolver/SafeContractResolver.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Scv.Api.Helpers.ContractResolver
+{
+    class SafeContractResolver : DefaultContractResolver
+    {
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            var jsonProp = base.CreateProperty(member, memberSerialization);
+            jsonProp.Required = Required.Default;
+            return jsonProp;
+        }
+    }
+}
--- api/Startup.cs
+++ api/Startup.cs
@@ -1,13 +1,13 @@
 using System;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.IO;
+using System.Reflection;
+using JCCommon.Clients.FileServices;
+using JCCommon.Framework;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
 using Scv.Api.Helpers.Middleware;
 
 namespace Scv.Api
@@ -34,8 +34,23 @@ namespace Scv.Api
                     builder.WithOrigins(corsDomain);
                 });
             });
-	    
-            services.AddControllers();
+
+            services.AddHttpClient<FileServicesClient>(client =>
+            {
+                client.DefaultRequestHeaders.Authorization = new BasicAuthenticationHeaderValue(
+                    Configuration.GetValue<string>("Fsc:Username"), Configuration.GetValue<string>("Fsc:Password"));
+            });
+
+            services.AddControllers().AddNewtonsoftJson();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.EnableAnnotations(true);
+                options.CustomSchemaIds(o => o.FullName);
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,7 +60,15 @@ namespace Scv.Api
             {
                 app.UseDeveloperExceptionPage();
             }
+
  	        app.UseCors();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "SCV.API");
+            });
 
             app.UseMiddleware(typeof(ErrorHandlingMiddleware));
 
@@ -54,7 +77,7 @@ namespace Scv.Api
             app.UseRouting();
 
             app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
