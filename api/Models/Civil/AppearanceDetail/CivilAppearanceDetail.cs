0c4169ea9db66ad8c32dc5aaed62e224a5538e52
Travis Semple <travis8814@gmail.com>
2020-05-19 11:15:25 -0700

    More model clean up

--- api/Models/Civil/AppearanceDetail/CivilAppearanceDetail.cs
+++ api/Models/Civil/AppearanceDetail/CivilAppearanceDetail.cs
@@ -1,21 +1,17 @@
 ﻿using System.Collections.Generic;
 using JCCommon.Clients.FileServices;
-using Newtonsoft.Json;
 
-namespace Scv.Api.Models.Civil.Detail
+namespace Scv.Api.Models.Civil.AppearanceDetail
 {
     public class CivilAppearanceDetail
     {
-        [JsonProperty("physicalFileId", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public string PhysicalFileId { get; set; }
 
-        [JsonProperty("party", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public ICollection<CivilAppearanceParty> Party { get; set; }
 
         /// <summary>
         /// Extended document object. 
         /// </summary>
-        [JsonProperty("document", Required = Required.DisallowNull, NullValueHandling = NullValueHandling.Ignore)]
         public ICollection<CivilAppearanceDocument> Document { get; set; }
 
         public ICollection<CivilAppearanceMethod> AppearanceMethod { get; set; }
