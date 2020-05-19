c50adeb5ccc2f2b102849ac14ac86e921ea2312a
Travis Semple <travis8814@gmail.com>
2020-05-19 11:15:57 -0700

    More model cleanup

--- api/Models/Criminal/Detail/CriminalDocument.cs
+++ api/Models/Criminal/Detail/CriminalDocument.cs
@@ -1,23 +1,15 @@
 ﻿using JCCommon.Clients.FileServices;
 
-namespace Scv.Api.Models.Criminal.Content
+namespace Scv.Api.Models.Criminal.Detail
 {
     /// <summary>
     /// Wrapper for CfcDocument, adding in additional fields
     /// </summary>
     public class CriminalDocument : CfcDocument
     {
-        [Newtonsoft.Json.JsonProperty("partId", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string PartId { get; set; }
-
-        [Newtonsoft.Json.JsonProperty("category", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string Category { get; set; }
-
-        [Newtonsoft.Json.JsonProperty("documentTypeDescription", Required = Newtonsoft.Json.Required.DisallowNull,
-            NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public string DocumentTypeDescription { get; set; }
-
-        [Newtonsoft.Json.JsonProperty("hasFutureAppearance", Required = Newtonsoft.Json.Required.DisallowNull, NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore)]
         public bool? HasFutureAppearance { get; set; }
     }
 }
