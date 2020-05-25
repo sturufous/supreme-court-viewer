c3d3ed44068635b76af347a2c78e190b3976e812
Marzieh Mehrnejad <marzmehr@uvic.ca>
2020-05-25 15:41:51 -0700

    scv-20: additional improvements to crown information, clean up of participants table code

--- web/src/components/criminal/CriminalCrownInformation.vue
+++ web/src/components/criminal/CriminalCrownInformation.vue
@@ -1,17 +1,42 @@
 <template>
 <body>
-   <b-card  v-if= "isMounted">       
-            <b-card bg-variant="light">
-                <b-table
-                :items="CrownInformation"
-                :fields="fields"
-                thead-class="d-none"
-                responsive="sm"               
-                striped
-                > 
-              
-                </b-table>
-            </b-card>       
+   <b-card  v-if= "isMounted">
+       <div>         
+            <h3 class="mx-2 font-weight-normal"> Crown Information </h3>
+            <hr class="mx-1 bg-light" style="height: 5px;"/>         
+        </div>       
+        <b-card bg-variant="white">
+            <b-table
+            :items="CrownInformation"
+            :fields="fields"            
+            thead-class="d-none"
+            responsive="sm"
+            borderless               
+            striped
+            >
+            <template  v-slot:cell(CrownInfoValue)="data">
+                <span v-if="data.item['CrownInfoFieldName'] == 'Crown Assigned' ">
+                    <b v-for="(assignee, index) in data.item['CrownInfoValue']"  v-bind:key="index" style= "white-space: pre-line">{{ assignee }} <br></b>
+                </span>
+                <span v-if="data.item['CrownInfoFieldName'] != 'Crown Assigned' ">
+                    <b > {{ data.value }}</b>
+                </span>                    
+            </template>  
+            <!-- <template v-for="(field,index) in fields" v-slot:[`cell(${field.key})`]="data">
+                <span v-bind:key="index" v-if="data.items.value != 'Crown Assigned'">
+                    <b > {{ data.value }}</b>
+                </span>
+            <span v-bind:key="index" v-if="data.items.value == 'Crown Assigned'">  
+                <p  
+                    v-for="(assigned,index) in assignedCrown"
+                    :key="index" 
+                    class="mr-1"> 
+                    {{ assigned }} 
+                </p>
+            </span>
+            </template> -->
+            </b-table>            
+        </b-card>       
    </b-card> 
 </body>
 </template>
@@ -28,25 +53,37 @@ export default class CriminalCrownInformation extends Vue {
     @criminalState.State
     public criminalFileInformation!: any
 
-    fields =[{key:"key", label: "Key"},
-    {key:"value", label: "Key", tdClass: 'crownInformation'}];
+    fields =[
+        {key:"CrownInfoFieldName", label: "Crown Info Field Name"},
+        {key:"CrownInfoValue", label: "Crown Info Value"}
+    ];
+
     public getCrownInfo(): void {
        
         const data = this.criminalFileInformation.detailsData;
-        let crownInfo = {};
-        // crownInfo["Crown Assigned"] = data.assignedPartNm;
-        // crownInfo["Crown Time Estimate"] = " Days"
-        // crownInfo["Case Age"] = data.caseAgeDays + " Days";
-        // crownInfo["Approved By"] = data.approvedByAgencyCd + "-"
-        // this.CrownInformation.push(crownInfo);                  
-        
+        const assignedCrown: string[] = [];
+        if (data.crown.length > 0) {
+            for (const assignee of data.crown) {
+                assignedCrown.push(assignee.lastNm + ", " + assignee.givenNm)
+            }
+            console.log(assignedCrown)
+        }
+        let crownInfo = {};    
 
-        crownInfo['key'] = "Crown Assigned";
-        crownInfo['value'] = data.assignedPartNm;
+        crownInfo['CrownInfoFieldName'] = "Crown Assigned";
+        crownInfo['CrownInfoValue'] = assignedCrown;
         this.CrownInformation.push(crownInfo);
         crownInfo = {};
-        crownInfo['key'] = "Crown Time Estimate";
-        crownInfo['value'] = " Days"
+        crownInfo['CrownInfoFieldName'] = "Crown Time Estimate";
+        crownInfo['CrownInfoValue'] = " Days"
+        this.CrownInformation.push(crownInfo);
+        crownInfo = {};
+        crownInfo['CrownInfoFieldName'] = "Case Age";
+        crownInfo['CrownInfoValue'] = data.caseAgeDays + " Days";
+        this.CrownInformation.push(crownInfo);
+        crownInfo = {};
+        crownInfo['CrownInfoFieldName'] = "Approved By";
+        crownInfo['CrownInfoValue'] = data.approvedByAgencyCd + "-"
         this.CrownInformation.push(crownInfo);  
 
         this.isMounted = true;
@@ -69,11 +106,6 @@ export default class CriminalCrownInformation extends Vue {
 </script>
 
 <style>
-
-    .crownInformation {
-        font-weight: bold;
-    }
-
     .card {
         border: white;
     }
