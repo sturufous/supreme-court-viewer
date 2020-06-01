9f983a7591ba5f75ac813c587f8960e444853cd8
marzmehr <marzmehr@uvic.ca>
2020-06-01 09:35:58 -0700

    scv-24: added the counts table to the criminal sentence page

--- web/src/components/criminal/CriminalSentenceDetails.vue
+++ web/src/components/criminal/CriminalSentenceDetails.vue
@@ -18,40 +18,65 @@
         </b-card>
 
         
-            <b-card bg-variant="light">           
-                <b-table
-                :items="participantFiles['Counts']"
-                :fields="fields"
-                :sort-by.sync="sortBy"
-                :sort-desc.sync="sortDesc"
-                :no-sort-reset="true"                
-                striped
-                borderless
-                sort-icon-left
-                responsive="sm"
-                >   
-                    <template v-for="(field,index) in fields" v-slot:[`head(${field.key})`]="data">
-                    <b v-bind:key="index" :class="field.headerStyle" > {{ data.label }}</b>
-                    </template>
-                    <template v-for="(field,index) in fields" v-slot:[`cell(${field.key})`]="data" >
-                        <span 
-                            v-bind:key="index"                                                    
-                            v-if="field.key.includes('Date')"                           
-                            :class= "cellClass(field, index, data)"  
-                            style="white-space: pre-line"> {{ data.value | beautify-date}}
-                        </span>
-                        <span 
-                            v-bind:key="index"                                                       
-                            v-else
-                            v-on:click= "cellClick(index, data)"
-                            :class= "cellClass(field, index, data)"  
-                            style="white-space: pre-line"> {{ data.value }}
-                        </span>
-                    </template>
-                </b-table>
-            </b-card>
-          
+            <b-card bg-variant="white">
+                
+                 <b-table-simple small responsive borderless>
+                    <b-thead head-variant="dark">                    
+                        <b-tr>
+                            <b-th v-for="(h,index) in fields" v-bind:key="index" >
+                                <b-icon-caret-down-fill v-if="index==0" @click="sortClick(h)"></b-icon-caret-down-fill>
+                                {{h.key}}
+                            </b-th>                        
+                        </b-tr>
+                    </b-thead>
 
+                    <b-tbody v-for="(counts,inx) in participantFiles[activeparticipant]['Counts']" v-bind:key="inx">
+                        <b-tr v-for="(sentence,index) in counts['Sentence/Disposition Type']" v-bind:key="index" :style ="getRowStyle(index)">                            
+                            
+                            <b-td :rowspan="counts.Len" v-if="index==0">{{counts['Date'] | beautify-date}}</b-td>                            
+                            <b-td :rowspan="counts.Len" v-if="index==0">{{counts['Count']}}</b-td>
+                            
+                            <b-td :rowspan="counts.Len" v-if="index==0">                                
+                                <b> {{counts.ChargeIssueCd}} </b> 
+                                <span>
+                                    &mdash; 
+                                    <b-badge
+                                        variant = "light"  
+                                        v-b-tooltip.hover 
+                                        :title="counts.ChargeIssueDscFull"> 
+                                        {{counts.ChargeIssueDsc}} 
+                                    </b-badge>
+                                </span>
+                            </b-td>
+                            
+                            <b-td :rowspan="counts.Len" v-if="index==0">
+                                <b-badge
+                                    variant = "light"  
+                                    v-b-tooltip.hover 
+                                    :title="counts.FindingDsc"> 
+                                    {{counts['Finding']}} 
+                                </b-badge>
+                            </b-td>
+
+                            <b-td>
+                                <b-badge
+                                    variant = "light"  
+                                    v-b-tooltip.hover 
+                                    title="counts.SentenceDsc"> 
+                                    {{sentence}} 
+                                </b-badge>
+                            </b-td>
+
+                            <b-td> {{counts['Term'][index]}} </b-td>
+                            <b-td> {{counts['Amount'][index]}} </b-td>
+                            <b-td> {{counts['Due Date/ Until'][index] | beautify-date }} </b-td>
+                            <b-td> {{counts['Effective Date'][index] | beautify-date }} </b-td>
+                           
+                        </b-tr>                       
+                    </b-tbody>
+                </b-table-simple>
+
+            </b-card>
    </b-card> 
 </body>
 </template>
@@ -72,11 +97,9 @@ export default class CriminalSentenceDetails extends Vue {
     public UpdateCriminalFile!: (newCriminalFileInformation: any) => void
 
 
-    public getParticipants(): void {
-       
+    public getParticipants(): void {       
         const data = this.criminalFileInformation.detailsData;
         this.participantJson = data.participant 
-        this.countJson = data.count
         this.ExtractParticipantInfo()          
         this.isMounted = true;
     }
@@ -86,34 +109,37 @@ export default class CriminalSentenceDetails extends Vue {
     }
 
     participantJson;
-    countJson;
-
+   
     activeparticipant = 0;           
-    sortBy = 'Date';
-    sortDesc = true;
+    sortBy = 'Count';
+    sortDesc = false;
    
     isMounted = false
-    
-    NumberOfCounts = 0;
 
     participantFiles: any[] = [];
 
-
     fields = [        
-            {key:'Date',                     sortable:true,   tdClass: 'border-top', headerStyle:'text-primary',  cellStyle:'text'},
-            {key:'Count',                    sortable:true,   tdClass: 'border-top', headerStyle:'text-primary',  cellStyle:'text-muted'},
-            {key:'Charge/Issue',             sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
-            {key:'Finding',                  sortable:true,   tdClass: 'border-top', headerStyle:'text-danger',   cellStyle:'text'},
-            {key:'Sentence/Disposition Type',sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
-            {key:'Term',                     sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
-            {key:'Amount',                   sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
-            {key:'Due Date/Until',           sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
-            {key:'Effective Date',           sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
-        ];
+        {key:'Date',                     sortable:true,   tdClass: 'border-top', headerStyle:'text-primary',  cellStyle:'text'},
+        {key:'Count',                    sortable:true,   tdClass: 'border-top', headerStyle:'text-primary',  cellStyle:'text-muted'},
+        {key:'Charge/Issue',             sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
+        {key:'Finding',                  sortable:true,   tdClass: 'border-top', headerStyle:'text-danger',   cellStyle:'text'},
+        
+        {key:'Sentence/ Disposition Type',sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
+   
+        {key:'Term',                     sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
+        {key:'Amount',                   sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
+        {key:'Due Date/ Until',          sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
+        {key:'Effective Date',           sortable:false,  tdClass: 'border-top', headerStyle:'text',          cellStyle:'text'},
+    ];
 
     public getNameOfParticipant(num)
     {        
-        return  this.participantFiles[num]["Last Name"]+', '+this.participantFiles[num]["First Name"];           
+        if(!this.participantFiles[num]["First Name"])
+            return  this.participantFiles[num]["Last Name"];
+        else if(!this.participantFiles[num]["Last Name"])
+            return this.participantFiles[num]["First Name"];
+        else
+            return  this.participantFiles[num]["Last Name"]+', '+this.participantFiles[num]["First Name"];           
     }
     
     public ExtractParticipantInfo(): void {        
@@ -123,33 +149,48 @@ export default class CriminalSentenceDetails extends Vue {
             const fileInfo = {};
             const jFile =  this.participantJson[fileIndex];
             fileInfo["Index"] = fileIndex; 
-            fileInfo["Part ID"] = jFile.partId;            
-            fileInfo["First Name"] = jFile.givenNm ? jFile.givenNm : "";
+            fileInfo["Part ID"] = jFile.partId;
+            fileInfo["First Name"] = jFile.givenNm.trim().length>0 ? jFile.givenNm : "";
             fileInfo["Last Name"] = jFile.lastNm ? jFile.lastNm : jFile.orgNm;            
             
             fileInfo["Counts"] = [];
             const counts: any[] = [];
            
-            //for(const cnt of jFile.count)
-            for(const cnt of  this.countJson)
+            for(const cnt of jFile.count)           
             {                
                 const countInfo = {}; 
-                countInfo["Date"]= cnt.appearanceDate? cnt.appearanceDate.split(' ')[0] : ''; 
-                countInfo["Count"]= cnt.countNumber;
-                countInfo["Charge/Issue"]= cnt.sectionTxt +'--' +cnt.sectionDscTxt;
+                countInfo["Date"] = cnt.appearanceDate? cnt.appearanceDate.split(' ')[0] : ''; 
+                countInfo["Count"] = cnt.countNumber;
+                countInfo["ChargeIssueCd"] = cnt.sectionTxt ;
+                countInfo["ChargeIssueDsc"] = cnt.sectionDscTxt.substr(0,20);
+                countInfo["ChargeIssueDscFull"] = cnt.sectionDscTxt;
                 countInfo["Finding"]= cnt.finding;
-                // countInfo["Sentence/Disposition Type"]=
-                // countInfo["Term"]=  
-                // countInfo["Amount"]=   
-                // countInfo["Due Date/Until"]=  
-                // countInfo["Effective Date"]=             
-                    
+                countInfo["FindingDsc"]= cnt.findingDsc;
+
+                countInfo["Sentence/Disposition Type"]=[];
+                countInfo["Term"] = [];
+                countInfo["Amount"]=[];
+                countInfo["Due Date/ Until"]=[];
+                countInfo["Effective Date"]=[]; 
+                countInfo["Len"]=cnt.sentence.length; 
+                
+                for(const sentence of cnt.sentence)
+                {                    
+                   countInfo["Sentence/Disposition Type"].push(sentence.sntpCd); 
+                   countInfo["Term"].push(sentence.sentTermPeriodQty? (sentence.sentTermPeriodQty + ' ' + sentence.sentTermCd):'')
+                   countInfo["Amount"].push(sentence.sentMonetaryAmt? sentence.sentMonetaryAmt:'')
+                   countInfo["Due Date/ Until"].push(sentence.sentDueTtpDt? sentence.sentDueTtpDt.split(' ')[0]:'')
+                   countInfo["Effective Date"].push(sentence.sentEffectiveDt?  sentence.sentEffectiveDt.split(' ')[0]:'')                   
+                } 
+                  
                 counts.push(countInfo);
             }
             fileInfo["Counts"] = counts;
                         
             this.participantFiles.push(fileInfo);
         }
+
+        console.log(this.participantFiles[this.activeparticipant]["Counts"])
         
     }
 
@@ -165,12 +206,19 @@ export default class CriminalSentenceDetails extends Vue {
         });        
     } 
 
-    get NumberOfDocuments() {       
-        
-        return(this.participantFiles[this.activeparticipant]["Documents"].length)            
-         
+    get NumberOfCounts() { 
+        return(this.participantFiles[this.activeparticipant]["Counts"].length) 
+    }
+
+    public getRowStyle(index) {
+        if(index==0) return "border-top : 1px solid #999;"
+        return '';
     }
     
+    public sortClick(data)
+    {
+        console.log('hi')
+    }
 }
 </script>
 
