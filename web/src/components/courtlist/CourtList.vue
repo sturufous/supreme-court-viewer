7035be9b58f96b19720ed5f408610ed3ddcc036f
marzmehr <marzmehr@uvic.ca>
2020-06-10 13:57:26 -0700

    scv-108: initial changes to add th court list page

---
+++ web/src/components/courtlist/CourtList.vue
@@ -0,0 +1,56 @@
+<template>
+<body>
+    <b-row cols="3">
+        <b-col md="4">
+            <b-form-group
+                label="Location *"
+                label-cols-sm="3"
+                label-align-sm="right"
+                label-size="md"
+                label-for="initialSortSelect"
+                class="mb-0"
+                >
+                <b-form-select
+                    v-model="sortDirection"
+                    id="initialSortSelect"
+                    size="sm"
+                    :options="['asc', 'desc', 'last']"
+                ></b-form-select>
+            </b-form-group>
+        </b-col>
+        <b-col md="2">
+             <!-- <b-form-group
+                id="input-group-1"
+                label="Email address:"
+                label-for="input-1"
+                description="We'll never share your email with anyone else.">                                            
+                    <b-form-input
+                    id="input-1"
+                    v-model="form.email"
+                    type="email"
+                    required
+                    placeholder="Enter email"
+                    ></b-form-input>
+            </b-form-group>  -->
+        </b-col>
+        <b-col md="2">
+            <b> jkdfvdh </b> 
+        </b-col>
+    </b-row>
+
+</body>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+import '@store/modules/CriminalFileInformation';
+const criminalState = namespace('CriminalFileInformation');
+
+@Component
+export default class CriminalCaseDetails extends Vue {
+
+
+}
+
+</script>
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -2,12 +2,19 @@ import { RouteConfig } from 'vue-router'
 import Home from '@components/Home.vue'
 import CivilCaseDetails from "@components/civil/CivilCaseDetails.vue";
 import CriminalCaseDetails from "@components/criminal/CriminalCaseDetails.vue";
+import CourtList from "@components/courtlist/CourtList.vue";
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/court-list',
+    name: 'CourtList',
+    component: CourtList, 
+    props: true
   },
   {
     path: '/civil-file/:fileNumber',
