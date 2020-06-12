83f70414c9556bb132246b591406f4cb025b39fb
Travis Semple <travis8814@gmail.com>
2020-06-12 13:41:55 -0700

    Include AppearanceReasonCd in court list

--- api/Models/Civil/CourtList/CivilCourtList.cs
+++ api/Models/Civil/CourtList/CivilCourtList.cs
@@ -13,6 +13,7 @@ namespace Scv.Api.Models.Civil.CourtList
         public string SupplementalEquipment { get; set; }
         public string SecurityRestriction { get; set; }
         public string AppearanceReasonDesc { get; set; }
+        public string AppearanceReasonCd { get; set; }
         public string JudgeInitials { get; set; }
 
         public new ICollection<ScheduledAppearance> ScheduledAppearance { get; set; }
