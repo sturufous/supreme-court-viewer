472af247feff0a4912798c67400d367b51ac054f
Travis Semple <travis8814@gmail.com>
2020-11-26 10:27:38 -0800

    Small merge.

--- web/src/main.ts
+++ web/src/main.ts
@@ -25,10 +25,6 @@ Vue.http.options.root = process.env.BASE_URL;
 if (location.pathname == "/")
 	history.pushState({ page: "home" }, "", process.env.BASE_URL);
 
-//Redirect from / to /scjscv/
-if (location.pathname == "/")
-	history.pushState({page: "home"}, "", process.env.BASE_URL)
-
 const router = new VueRouter({
 	mode: 'history',
 	base: process.env.BASE_URL,
