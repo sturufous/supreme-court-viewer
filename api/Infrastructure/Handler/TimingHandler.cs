a6df21acbcde41327ee87e61a52e0fdc70e715af
Travis Semple <travis8814@gmail.com>
2021-07-21 14:39:07 -0700

    Small comment update.

--- api/Infrastructure/Handler/TimingHandler.cs
+++ api/Infrastructure/Handler/TimingHandler.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 
 namespace Scv.Api.Infrastructure.Handler
 {
+    /// <summary>
+    /// This is created so we can measure the time between request and responses. There doesn't seem to be a very easy way of doing this in .NET Core. 
+    /// I tried enabling scopes, but they seemed to print the same TraceId/SpanId/ConnectionId/RequestId. 
+    /// </summary>
     public class TimingHandler : DelegatingHandler
     {
         private readonly ILogger<TimingHandler> _logger;
