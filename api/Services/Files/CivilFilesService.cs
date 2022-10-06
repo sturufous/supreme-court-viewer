df5fd68a2aabdae2f87a40e5c59d8079de8f2541
nikola-rados <nrados@uvic.ca>
2022-10-06 11:56:47 -0700

    Fix logical typo

--- api/Services/Files/CivilFilesService.cs
+++ api/Services/Files/CivilFilesService.cs
@@ -323,7 +323,7 @@ namespace Scv.Api.Services.Files
                 document.FiledBy = documentFromFileContent?.FiledBy;
                 document.Category = _lookupService.GetDocumentCategory(document.DocumentTypeCd);
                 document.DocumentTypeDescription = await _lookupService.GetDocumentDescriptionAsync(document.DocumentTypeCd);
-                document.ImageId = document.SealedYN == "N" ? null : document.ImageId;
+                document.ImageId = document.SealedYN != "N" ? null : document.ImageId;
                 document.NextAppearanceDt = document.Appearance?.Where(app => DateTime.TryParse(app?.AppearanceDate, out DateTime appearanceDate) && appearanceDate >= DateTime.Today).FirstOrDefault()?.AppearanceDate;
                 document.Appearance = null;
                 document.SwornByNm = documentFromFileContent.SwornByNm;
