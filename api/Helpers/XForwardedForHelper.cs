5437d223fd995dae1f08f820b0449ddb4e36a6da
Stuart Morse <stuart.morse@quartech.com>
2024-06-07 11:24:33 -0700

    SCV-459 Added github actions for deploying to the Emerald cluster

--- api/Helpers/XForwardedForHelper.cs
+++ api/Helpers/XForwardedForHelper.cs
@@ -4,7 +4,8 @@
     {
         public static string BuildUrlString(string forwardedHost, string forwardedPort, string baseUrl)
         {
-            var portComponent = string.IsNullOrEmpty(forwardedPort) || forwardedPort == "80" || forwardedPort == "443" ? "" : $":{forwardedPort}";
+            //var portComponent = string.IsNullOrEmpty(forwardedPort) || forwardedPort == "80" || forwardedPort == "443" ? "" : $":{forwardedPort}";
+            var portComponent = string.IsNullOrEmpty(forwardedPort) || forwardedPort == "80" || forwardedPort == "443" || forwardedPort == "8080" ? "" : $":{forwardedPort}";
             return $"https://{forwardedHost}{portComponent}{baseUrl}";
         }
     }
